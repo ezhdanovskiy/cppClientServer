f6ad4f434c2fe2cdd0490ed5dbcc984efea0b554
Evgeny Zhdanovskiy <zhdanovskiy@playform.com>
2016-08-15 21:14:17 +0300

    HTTPEpollServer : add BaseController

--- HTTPEpollServer/HTTPEpollServer.cpp
+++ HTTPEpollServer/HTTPEpollServer.cpp
@@ -11,9 +11,10 @@
 #include <defs.h>
 #include <sstream>
 
-std::map<int, FDType> fdMap;
+int events_fd;
 
 int setNonblocking(int fd) {
+    LOG(__func__ << "(" << fd << ")");
     int flags = 0;
     if (-1 == (flags = fcntl(fd, F_GETFL, 0))) {
         flags = 0;
@@ -21,16 +22,128 @@ int setNonblocking(int fd) {
     return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
 }
 
-int epollCtlAdd(int epfd, int listen_fd, unsigned int events) {
-    struct epoll_event e;
-    e.data.fd = listen_fd;
-    e.events = events;
-    return epoll_ctl(epfd, EPOLL_CTL_ADD, listen_fd, &e);
+std::ostream& operator<<(std::ostream &o, const epoll_event &ev) {
+    return o << "epoll_event{ptr=" << std::hex << ev.data.ptr << " events=" << ev.events << std::dec << "}";
 }
 
-std::ostream& operator<<(std::ostream &o, const epoll_event &ev) {
-    return o << "epoll_event{fd=" << ev.data.fd << " events=" << std::hex << ev.events << std::dec << "}";
+class BaseController {
+protected:
+    int fd;
+public:
+    BaseController(int fd) : fd(fd) {
+        LOG(__func__ << "()");
+    }
+
+    int getFD() {
+        return fd;
+    }
+
+    virtual int dispatch(const epoll_event &event) = 0;
+
+    virtual ~BaseController() {
+        LOG(__func__);
+        if (fd) {
+            close(fd);
+        }
+    }
+};
+
+int epollCtlAdd(int epfd, BaseController *ptr, unsigned int events) {
+    LOG(__func__ << "(ptr->getFD()=" << ptr->getFD() << ")");
+    struct epoll_event e;
+    e.data.ptr = ptr;
+    e.events = events;
+    return epoll_ctl(epfd, EPOLL_CTL_ADD, ptr->getFD(), &e);
 }
+
+class SocketController : public BaseController {
+public:
+    SocketController(int fd) : BaseController(fd) {
+        LOG(__func__ << "()");
+    }
+
+    int dispatch(const epoll_event &event) override {
+        LOG("SocketController::" << __func__ << "(" << event << ")");
+        if(event.events & EPOLLRDHUP) {
+            LOG("  close(" << fd << ")");
+            close(fd);
+            return 0;
+        }
+        if (event.events & EPOLLIN) {
+            char buffer[BUFFER_SIZE];
+            long received = recv(fd, buffer, BUFFER_SIZE, 0);
+            if (received < 0) {
+                warn("Error reading from socket \t%s:%d", __FILE__, __LINE__);
+                return -1;
+            }
+            if (received == 0) {
+                LOG("  close(" << fd << ")");
+                close(fd);
+                return 0;
+            } else {
+                buffer[received] = 0;
+                LOG("  Read " << received << " bytes: '\033[1m" << buffer << "\033[0m'");
+            }
+
+            std::string ansHeader = "HTTP/1.1 200 OK\n\n";
+            long sentAll = send(fd, ansHeader.c_str(), ansHeader.size(), 0);
+            LOG("  send " << sentAll << " bytes of " << ansHeader.size() << ": '\033[1m" << ansHeader << "\033[0m'");
+
+            std::stringstream ansBody;
+            ansBody << "<!DOCTYPE HTML><html><head><meta charset=\"utf-8\"><title>HTTPEpollServer</title></head><body>";
+            for (long j = 1; j < 1000000; ++j) {
+                ansBody << j + 1000000000 << "<br>\n";
+            }
+            ansBody << "</body></html>";
+
+            long sentBody = 0;
+            while (sentBody < ansBody.str().size()) {
+                long sent = send(fd, ansBody.str().c_str() + sentBody, ansBody.str().size() - sentBody, 0);
+                CHECK_LONG_ERR(sent);
+                sentBody += sent;
+                sentAll += sent;
+                LOG("  send " << sentAll << " bytes of " << ansBody.str().size() + ansHeader.size() << " bytes");
+//                usleep(500000);
+            }
+
+            LOG("  epoll_ctl(" << events_fd << ", EPOLL_CTL_DEL, " << fd << ", NULL)");
+            epoll_ctl(events_fd, EPOLL_CTL_DEL, fd, NULL);
+
+            LOG("  close(" << fd << ")");
+            close(fd);
+        }
+        if (event.events & EPOLLERR) {
+            err(1, "\t%s:%d", __FILE__, __LINE__);
+        }
+    };
+};
+
+class AcceptController : public BaseController {
+public:
+    AcceptController(int fd) : BaseController(fd) {
+        LOG(__func__ << "()");
+    }
+
+    int dispatch(const epoll_event &event) override {
+        LOG("AcceptController::" << __func__ << "(" << event << ")");
+        struct sockaddr_in client_addr;
+        socklen_t ca_len = sizeof(client_addr);
+        int client_fd = accept(fd, (struct sockaddr *) &client_addr, &ca_len);
+        LOG("  accept(" << fd << ") return " << client_fd);
+        if (client_fd < 0) {
+            warn("Error accepting \t%s:%d", __FILE__, __LINE__);
+            return -1;
+        }
+        LOG("  Client connected: " << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port));
+        setNonblocking(client_fd);
+
+        if (epollCtlAdd(events_fd, new SocketController(client_fd), EPOLLIN|EPOLLRDHUP) < 0) {
+            LOG("ERROR");
+            err(1, "\t%s:%d", __FILE__, __LINE__);
+        }
+        return 0;
+    };
+};
 
 int main(int argc, char **argv) {
     int port = 22000;
@@ -41,11 +154,10 @@ int main(int argc, char **argv) {
     }
     LOG("LISTEN " << host << ":" << port);
 
-    int events_fd = epoll_create(MAX_EVENTS);
+    events_fd = epoll_create(MAX_EVENTS);
     if (events_fd < 0) {
         err(1, "\t%s:%d", __FILE__, __LINE__);
     }
-    fdMap[events_fd] = FDType::events;
 
     struct sockaddr_in servaddr;
     bzero(&servaddr, sizeof(servaddr));
@@ -57,7 +169,6 @@ int main(int argc, char **argv) {
     if (listen_fd < 0) {
         err(1, "\t%s:%d", __FILE__, __LINE__);
     }
-    fdMap[listen_fd] = FDType::listen;
 
     int enable = 1;
     if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0) {
@@ -72,94 +183,21 @@ int main(int argc, char **argv) {
     }
     setNonblocking(listen_fd);
 
-    if (epollCtlAdd(events_fd, listen_fd, EPOLLIN|EPOLLRDHUP) < 0) {
+    if (epollCtlAdd(events_fd, new AcceptController(listen_fd), EPOLLIN|EPOLLRDHUP) < 0) {
         err(1, "\t%s:%d", __FILE__, __LINE__);
     }
 
     struct epoll_event events[MAX_EVENTS];
-    while (1) {
+    int iterations = 6;
+    while (iterations--) {
         int events_size = epoll_wait(events_fd, events, MAX_EVENTS, -1 /* Timeout */);
 
         for (int i = 0; i < events_size; ++i) {
             const epoll_event &event = events[i];
             LOG(event);
-            int fd = event.data.fd;
-            auto fdMapIt = fdMap.find(fd);
-            if (fdMapIt == fdMap.end()) {
-                warn("Error accepting \t%s:%d", __FILE__, __LINE__);
-                continue;
-            }
-            switch (fdMapIt->second) {
-                case FDType::listen: {
-                    struct sockaddr_in client_addr;
-                    socklen_t ca_len = sizeof(client_addr);
-                    int client_fd = accept(fd, (struct sockaddr *) &client_addr, &ca_len);
-                    LOG("accept(" << fd << ") return " << client_fd);
-                    if (client_fd < 0) {
-                        warn("Error accepting \t%s:%d", __FILE__, __LINE__);
-                        continue;
-                    }
-                    LOG("Client connected: " << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port));
-                    fdMap[client_fd] = FDType::client;
-                    setNonblocking(client_fd);
+            BaseController *controller = (BaseController*)event.data.ptr;
+            controller->dispatch(event);
 
-                    if (epollCtlAdd(events_fd, client_fd, EPOLLIN|EPOLLRDHUP) < 0) {
-                        err(1, "\t%s:%d", __FILE__, __LINE__);
-                    }
-                    break;
-                }
-                case FDType::client: {
-                    if(event.events & EPOLLRDHUP) {
-                        LOG("close(" << fd << ")");
-                        close(fd);
-                        break;
-                    }
-                    if (event.events & EPOLLIN) {
-                        char buffer[BUFFER_SIZE];
-                        long received = recv(fd, buffer, BUFFER_SIZE, 0);
-                        if (received < 0) {
-                            warn("Error reading from socket \t%s:%d", __FILE__, __LINE__);
-                            break;
-                        }
-                        if (received == 0) {
-                            LOG("close(" << fd << ")");
-                            close(fd);
-                            break;
-                        } else {
-                            buffer[received] = 0;
-                            LOG("Read " << received << " bytes: '\033[1m" << buffer << "\033[0m'");
-                        }
-
-                        std::string ansHeader = "HTTP/1.1 200 OK\n\n";
-                        long sentAll = send(fd, ansHeader.c_str(), ansHeader.size(), 0);
-                        LOG("send " << sentAll << " bytes of " << ansHeader.size() << ": '\033[1m" << ansHeader << "\033[0m'");
-
-                        std::stringstream ansBody;
-                        ansBody << "<!DOCTYPE HTML><html><head><meta charset=\"utf-8\"><title>HTTPEpollServer</title></head><body>";
-                        for (long j = 1; j < 10000000; ++j) {
-                            ansBody << j + 1000000000 << "<br>\n";
-                        }
-                        ansBody << "</body></html>";
-
-                        long sentBody = 0;
-                        while (sentBody < ansBody.str().size()) {
-                            long sent = send(fd, ansBody.str().c_str() + sentBody, ansBody.str().size() - sentBody, 0);
-                            CHECK_INT_ERR(sent);
-                            sentBody += sent;
-                            sentAll += sent;
-                            LOG("send " << sentAll << " bytes of " << ansBody.str().size() + ansHeader.size() << " bytes");
-                            usleep(500000);
-                        }
-
-                        LOG("close(" << fd << ")");
-                        close(fd);
-                    }
-                    if (event.events & EPOLLERR) {
-                        err(1, "\t%s:%d", __FILE__, __LINE__);
-                    }
-                    break;
-                }
-            }
         }
     }
     close(events_fd);
--- include/defs.h
+++ include/defs.h
@@ -5,8 +5,8 @@
 #include <err.h>
 #include <stdlib.h>
 
-#define LOG(chain) std::cout << chain << "\t" << __FILE__ << ":" << __LINE__ <<std::endl;
-#define LOG1(var) std::cout << #var << "=" << var << "\t" << __FILE__ << ":" << __LINE__ <<std::endl;
+#define LOG(chain) std::cout << chain << "\t\t" << __FILE__ << ":" << __LINE__ <<std::endl;
+#define LOG1(var) std::cout << #var << "=" << var << "\t\t" << __FILE__ << ":" << __LINE__ <<std::endl;
 #define CHECK_INT_ERR(var) if (var < 0) { err(1, "%s = %d\t%s:%d", #var, var, __FILE__, __LINE__); }
 #define CHECK_LONG_ERR(var) if (var < 0) { err(1, "%s = %ld\t%s:%d", #var, var, __FILE__, __LINE__); }
 
